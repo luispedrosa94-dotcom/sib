7eef8ff8fff25c91f7d13e95eb150d611253e174
O teu nome <o.teu.email@example.com>
2026-09-23 20:52:54 +0100

    Update package authorship

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
+__author__ = "Luis Pedrosa"
 __credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "Luis Pedrosa"
+__email__ = "pg58709@uminho.pt"
 
 
 
